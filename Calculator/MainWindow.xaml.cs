1b2090c54350092af3e305d3caec5b32907dcd5a
dan10097110 <danhog@outlook.de>
2016-07-03 21:13:30 +0200

    more functions

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,205 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastAnswer;
+        int caretIndex;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void Write(string s)
+        {
+            caretIndex = textBox.CaretIndex;
+            textBox.Text = textBox.Text.Insert(caretIndex, s);
+            textBox.CaretIndex += caretIndex + s.Length;
+            textBox1.Text = "";
+        }
+
+        void Write(string s, int skip)
+        {
+            caretIndex = textBox.CaretIndex;
+            textBox.Text = textBox.Text.Insert(caretIndex, s);
+            textBox.CaretIndex += caretIndex + skip;
+            textBox1.Text = "";
+        }
+
+        private void buttonSinus_Click(object sender, RoutedEventArgs e)
+        {
+            Write("sin(°)", 4);
+        }
+
+        private void buttonTangens_Click(object sender, RoutedEventArgs e)
+        {
+            Write("tan(°)", 4);
+        }
+
+        private void buttonCosinus_Click(object sender, RoutedEventArgs e)
+        {
+            Write("cos(°)", 4);
+        }
+
+        private void buttonLastAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            Write(lastAnswer.ToString());
+        }
+
+        private void buttonOffeneKlammer_Click(object sender, RoutedEventArgs e)
+        {
+            Write("(");
+        }
+
+        private void buttonFakultät_Click(object sender, RoutedEventArgs e)
+        {
+            Write("!");
+        }
+
+        private void buttonLogarithmus_Click(object sender, RoutedEventArgs e)
+        {
+            Write("log()", 4);
+        }
+
+        private void buttonPi_Click(object sender, RoutedEventArgs e)
+        {
+            Write("π");
+        }
+
+        private void buttonGeschlosseneKlammer_Click(object sender, RoutedEventArgs e)
+        {
+            Write(")");
+        }
+
+        private void buttonProzent_Click(object sender, RoutedEventArgs e)
+        {
+            Write("%");
+        }
+
+        private void buttonNatürlicherLogarithmus_Click(object sender, RoutedEventArgs e)
+        {
+            Write("ln()", 3);
+        }
+
+        private void buttonE_Click(object sender, RoutedEventArgs e)
+        {
+            Write("e");
+        }
+
+        private void buttonSieben_Click(object sender, RoutedEventArgs e)
+        {
+            Write("7");
+        }
+
+        private void buttonVier_Click(object sender, RoutedEventArgs e)
+        {
+            Write("4");
+        }
+
+        private void buttonEins_Click(object sender, RoutedEventArgs e)
+        {
+            Write("1");
+        }
+
+        private void buttonNull_Click(object sender, RoutedEventArgs e)
+        {
+            Write("0");
+        }
+
+        private void buttonAcht_Click(object sender, RoutedEventArgs e)
+        {
+            Write("8");
+        }
+
+        private void buttonFünf_Click(object sender, RoutedEventArgs e)
+        {
+            Write("5");
+        }
+
+        private void buttonZwei_Click(object sender, RoutedEventArgs e)
+        {
+            Write("2");
+        }
+
+        private void buttonKomma_Click(object sender, RoutedEventArgs e)
+        {
+            Write(",");
+        }
+
+        private void buttonNeun_Click(object sender, RoutedEventArgs e)
+        {
+            Write("9");
+        }
+
+        private void buttonSechs_Click(object sender, RoutedEventArgs e)
+        {
+            Write("6");
+        }
+
+        private void buttonDrei_Click(object sender, RoutedEventArgs e)
+        {
+            Write("3");
+        }
+
+        private void buttonGleich_Click(object sender, RoutedEventArgs e)
+        {
+            double ergebnis = Math.Calculate(textBox.Text);
+            textBox1.Text = ergebnis.ToString();
+            lastAnswer = ergebnis;
+        }
+
+        private void buttonClearElement_Click(object sender, RoutedEventArgs e)
+        {
+            if(textBox.Text != "")
+            {
+                caretIndex = textBox.CaretIndex;
+                textBox1.Text = "";
+                if (caretIndex == 0)
+                {
+                    textBox.Text = textBox.Text.Remove(caretIndex, 1);
+                    textBox.CaretIndex = caretIndex;
+                }
+                else
+                {
+                    textBox.Text = textBox.Text.Remove(caretIndex - 1, 1);
+                    textBox.CaretIndex = caretIndex - 1;
+                }
+            }
+        }
+
+        private void buttonHoch_Click(object sender, RoutedEventArgs e)
+        {
+            Write("^");
+        }
+
+        private void buttonMal_Click(object sender, RoutedEventArgs e)
+        {
+            Write("*");
+        }
+
+        private void buttonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Write("+");
+        }
+
+        private void buttonAllClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void buttonWurzel_Click(object sender, RoutedEventArgs e)
+        {
+            Write("√");
+        }
+
+        private void buttonGeteilt_Click(object sender, RoutedEventArgs e)
+        {
+            Write("/");
+        }
+
+        private void buttonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Write("-");
         }
     }
 }
---
+++ Calculator/Math.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
+{
+    static class Math
+    {
+        public static double Calculate(string therm)
+        {
+            return new Therm(therm).GetErgebnis();
+        }
+    }
+
+    class Therm
+    {
+        string therm;
+        Therm first, second;
+        byte rechenart = 0;
+        List<string> klammerContent = new List<string>();
+        double ergebnis;
+
+        public Therm(string therm)
+        {
+            //prozent wird unsicher ersetzt
+            therm = therm.Replace("%", "/100").Replace("π", System.Math.PI.ToString()).Replace("e", System.Math.E.ToString());
+            this.therm = therm;
+            SplitTherm();
+            Calculate();
+        }
+
+        public ulong Fakultät(ulong n)
+        {
+            ulong result = 1;
+            for (ulong i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public double GetErgebnis()
+        {
+            return ergebnis;
+        }
+
+        void Calculate()
+        {
+            switch (rechenart)
+            {
+                case 1: ergebnis = first.GetErgebnis() + second.GetErgebnis(); break;
+                case 2: ergebnis = first.GetErgebnis() - second.GetErgebnis(); break;
+                case 3: ergebnis = first.GetErgebnis() * second.GetErgebnis(); break;
+                case 4: ergebnis = first.GetErgebnis() / second.GetErgebnis(); break;
+                case 5: ergebnis = System.Math.Pow(first.GetErgebnis(), second.GetErgebnis()); break;
+                case 6: ergebnis = System.Math.Pow(second.GetErgebnis(), 1 / first.GetErgebnis()); break;
+                default: ergebnis = Convert.ToDouble(therm); break;
+            }
+        }
+
+        bool AInfrontOfB(string s, string a, string b)
+        {
+            return s.Contains(a) && (s.IndexOf(a) < s.IndexOf(b) || !s.Contains(b));
+        }
+
+        string FakultätBerechnen(string stringtherm)
+        {
+            while(stringtherm.Contains("!"))
+            {
+                int i = stringtherm.IndexOf("!");
+                string s = stringtherm.Remove(i);
+                s = ReverseString(s);
+                List<int> pos = new List<int>();
+                if (s.Contains("+"))
+                    pos.Add(s.IndexOf("+"));
+                else if (s.Contains("-"))
+                    pos.Add(s.IndexOf("-"));
+                else if (s.Contains("*"))
+                    pos.Add(s.IndexOf("*"));
+                else if (s.Contains("/"))
+                    pos.Add(s.IndexOf("/"));
+                string smallTherm = "";
+                if (pos.Count == 0)
+                {
+                    smallTherm = ReverseString(s);
+                    ulong fakultät = Fakultät((ulong)(new Therm(smallTherm).GetErgebnis()));
+                    stringtherm = stringtherm.Remove(0, i + 1);
+                    stringtherm = stringtherm.Insert(0, fakultät.ToString());
+                }
+                else
+                {
+                    int smallest = int.MaxValue;
+                    foreach (int p in pos)
+                        smallest = System.Math.Min(smallest, p);
+                    smallTherm = ReverseString(s.Remove(smallest));
+                    ulong fakultät = Fakultät((ulong)(new Therm(smallTherm).GetErgebnis()));
+                    stringtherm = stringtherm.Remove(smallest + 1, i - smallest);
+                    stringtherm = stringtherm.Insert(smallest + 1, fakultät.ToString());
+                }
+            }
+            return stringtherm;
+        }
+
+        void SplitTherm()
+        {
+            string rTherm = ReverseString(ReplaceKlammern(therm));
+            rTherm = ReverseString(FakultätBerechnen(ReverseString(rTherm)));
+            therm = ReverseString(ReplaceKlammern(InsertContent(rTherm)));
+            for (;;)
+            {
+                if (rTherm != "c")
+                {
+                    byte pos = 0;
+                    if (AInfrontOfB(rTherm, "+", "-"))
+                    {
+                        rechenart = 1;
+                        pos = (byte)rTherm.IndexOf('+');
+                    }
+                    else if (rTherm.Contains('-'))
+                    {
+                        rechenart = 2;
+                        pos = (byte)rTherm.IndexOf('-');
+                    }
+                    else if (AInfrontOfB(rTherm, "*", "/"))
+                    {
+                        rechenart = 3;
+                        pos = (byte)rTherm.IndexOf('*');
+                    }
+                    else if (rTherm.Contains('/'))
+                    {
+                        rechenart = 4;
+                        pos = (byte)rTherm.IndexOf('/');
+                    }
+                    else if (AInfrontOfB(rTherm, "^", "√"))
+                    {
+                        rechenart = 5;
+                        pos = (byte)rTherm.IndexOf('^');
+                    }
+                    else if(rTherm.Contains('√'))
+                    {
+                        rechenart = 6;
+                        pos = (byte)rTherm.IndexOf('√');
+                    }
+                    if(rechenart != 0)
+                    {
+                        first = new Therm(InsertContent(ReverseString(rTherm.Substring(pos + 1))));
+                        second = new Therm(InsertContent(ReverseString(rTherm.Remove(pos))));
+                    }
+                    break;
+                }
+                else
+                {
+                    rTherm = InsertContent(rTherm);
+                    rTherm = rTherm.Remove(0, 1);
+                    rTherm = rTherm.Remove(rTherm.Length - 1, 1);
+                    therm = ReverseString(ReplaceKlammern(rTherm));
+                }
+            }
+        }
+
+        string ReplaceKlammern(string s)
+        {
+            while (s.Contains('(') || s.Contains(')'))
+            {
+                byte i = 0, posKlammerAuf = 0, posKlammerZu = 0;
+                do
+                {
+                    if (AInfrontOfB(s, "(", ")"))
+                    {
+                        byte posA = (byte)s.IndexOf('(');
+                        s = s.Remove(posA, 1);
+                        s = s.Insert(posA, "a");
+                        if (i == 0) posKlammerAuf = posA;
+                        i++;
+                    }
+                    else
+                    {
+                        byte posZ = (byte)s.IndexOf(')');
+                        s = s.Remove(posZ, 1);
+                        s = s.Insert(posZ, "z");
+                        i--;
+                        if (i == 0) posKlammerZu = posZ;
+                    }
+                } while (i != 0);
+                s = s.Replace('a', '(').Replace('z', ')');
+                klammerContent.Add(s.Substring(posKlammerAuf, posKlammerZu - posKlammerAuf + 1));
+                s = s.Remove(posKlammerAuf, posKlammerZu - posKlammerAuf + 1);
+                s = s.Insert(posKlammerAuf, "c");
+            }
+            return s;
+        }
+
+        string InsertContent(string therm)
+        {
+            while (therm.Contains('c'))
+            {
+                byte pos = (byte)therm.IndexOf('c');
+                therm = therm.Remove(pos, 1);
+                therm = therm.Insert(pos, klammerContent.First());
+                klammerContent.Remove(klammerContent.First());
+            }
+            return therm;
+        }
+
+        string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
